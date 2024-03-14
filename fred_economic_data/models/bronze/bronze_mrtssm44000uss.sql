9d2ffb46e50eba3fa860d1e1f6c6ab5a049d5464
jeffreyablack1 <jeffreyablack1@gmail.com>
2024-03-13 19:44:29 -0500

    Created Bronze Layer Data

---
+++ fred_economic_data/models/bronze/bronze_mrtssm44000uss.sql
@@ -0,0 +1,10 @@
+SELECT 
+date AS Date,
+series_index AS Series,
+name AS Name,
+units AS Units,
+seasonal_adjustments AS AdjSeas,
+frequency AS Freq,
+fred_last_updated_date AS LastUpdatedDate
+fetch_date AS FetchDate
+FROM mrtssm44000uss
